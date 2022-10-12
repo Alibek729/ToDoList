609659f9cccde492b479450fbdd18bb47ce40f3d
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2022-10-12 23:54:21 +0600

    Added Bar button action "add button pressed", which allows to add new items to my array of string

--- ToDoList/TodoListViewController.swift
+++ ToDoList/TodoListViewController.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demodragon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demodragon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     //MARK: - Add new button
-
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New item to List", message: nil, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension TodoListViewController {
